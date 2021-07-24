080a4b6ab5689d7ba8609e4c6c32ab2efa5d64eb
Uptimum <uptimumdn2000@gmail.com>
2021-07-24 11:45:56 +0700

    .

--- public/js/index.js
+++ public/js/index.js
@@ -15,10 +15,21 @@ $(document).ready(() => {
         },
         speed: 800,
     })
+    //
     Array.from($('.sidebar ul li span')).forEach(element => {
         $(element).click(() => {
             $(element).next('.sub-menu').slideToggle();
             $(element).children().toggleClass('rotate')
+        })
+    })
+    //
+    const modes = Array.from($('.product .row .col-md-9 .toolbar .row .modes span'))
+    modes.forEach((mode) => {
+        $(mode).click(() => {
+            modes.forEach((element) => {
+                $(element).removeClass('active')
+            })
+            $(mode).addClass('active')
         })
     })
 })
