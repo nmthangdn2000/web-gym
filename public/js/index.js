14fec9b9c0e81fc8e3472d1fd61005817b14d64d
Uptimum <uptimumdn2000@gmail.com>
2021-07-31 17:20:10 +0700

    .

--- public/js/index.js
+++ public/js/index.js
@@ -23,42 +23,64 @@ $(document).ready(() => {
         })
     })
     //
-    const modes = Array.from($('.product .row .col-md-9 .toolbar .row .modes span'))
-    modes.forEach((mode) => {
-        $(mode).click(() => {
-            modes.forEach((element) => {
-                $(element).removeClass('active')
-            })
-            $(mode).addClass('active')
+    setTimeout(()=>{
+        $('#loading-page').animate({opacity: 0}, 1000, () => {
+            $('#loading-page').addClass('hide').delay(400)
         })
-    })
+    }, 200)
     //
     $(window).on('scroll', () => {
-        if ($(window).scrollTop() > 115){
+        if ($(window).scrollTop() > 115) {
             $('header .fixed').addClass('fixed-nav')
             $('header .fixed').css('top', '0')
             $('.container').css('padding-top', '7.75rem')
         }
-        else{
+        else {
             $('header .fixed').removeClass('fixed-nav')
             $('header .fixed').css('top', '-80%')
             $('.container').css('padding-top', '')
         }
     })
     $('#btn-toggler').on('click', () => {
-        $('.header-top .row nav').addClass('active')    
-        $('#cover').addClass('cover')      
+        $('.header-top .row nav').addClass('active')
+        $('#cover').addClass('cover')
         $('.search-box.search-box-bg').removeClass('active')
     })
     $('#close-nav').on('click', () => {
         $('.header-top .row nav').removeClass('active')
-        $('#cover').removeClass('cover')          
+        $('#cover').removeClass('cover')
     })
     $('#cover').on('click', () => {
         $('.header-top .row nav').removeClass('active')
-        $('#cover').removeClass('cover')   
+        $('#cover').removeClass('cover')
     })
     $('#btn-open-search-box').on('click', () => {
         $('.search-box.search-box-bg').toggleClass('active')
     })
+    //
+    $('#modes-grid').on('click', () => {
+        localStorage.setItem('modes', 'grids')
+        modesGrid()
+    })
+    $('#modes-column').on('click', () => {
+        localStorage.setItem('modes', 'columns')
+        modesColumn()
+    })
+    // local storage
+    const modesListProduct = localStorage.getItem('modes') || 'grids'
+    if (modesListProduct == 'grids') {
+        modesGrid()
+    } else {
+        modesColumn()
+    }
 })
+function modesGrid() {
+    $('#modes-grid').addClass('active')
+    $('#modes-column').removeClass('active')
+    $('#product-list').removeClass('list-column')
+}
+function modesColumn() {
+    $('#modes-column').addClass('active')
+    $('#modes-grid').removeClass('active')
+    $('#product-list').addClass('list-column')
+}
